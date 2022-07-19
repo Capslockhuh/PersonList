0018437e99adad8a6f5b1eda7553fd482360f428
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-19 09:42:59 +0200

    Initial Commit

---
+++ PersonList/PersonListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonListApp.swift
+//  PersonList
+//
+//  Created by Jan Andrzejewski on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
