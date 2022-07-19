9b15d40761c8f54278ae21e40988d8c0dab85bf1
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-19 10:01:21 +0200

    App now shows the selected image on the screen

--- PersonList/ContentView.swift
+++ PersonList/ContentView.swift
@@ -9,10 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingImagePicker = false
+    
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
     var body: some View {
-        Button("test") {
-
+        NavigationView {
+            List {
+                VStack {
+                    Button("test") {
+                        showingImagePicker = true
+                    }
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            .navigationTitle("PersonList")
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+            .onChange(of: inputImage) { _ in loadImage() }
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else {
+            return
+        }
+        
+        image = Image(uiImage: inputImage)
     }
 }
 
